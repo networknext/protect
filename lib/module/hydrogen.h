efe68795ae2e0bad650fc4908187617ca5c73b87
Glenn Fiedler <glenn@networknext.com>
2025-11-04 10:16:17 -0500

    again

--- lib/module/hydrogen.h
+++ lib/module/hydrogen.h
@@ -27,15 +27,15 @@
 #endif
 
 #if defined(__linux__) && defined(__KERNEL__)
-typedef uint64 __u64
-typedef uint32 __u32
-typedef uint16 __u16
-typedef uint8 __u8
-typedef int64 __i64
-typedef int32 __i32
-typedef int16 __i16
-typedef int8 __i8
-typedef size_t __u64
+typedef __u64 uint64_t
+typedef __u32 uint32_t
+typedef __u16 uint16_t
+typedef __u8  uint8_t
+typedef __i64 int64_t 
+typedef __i32 int32_t
+typedef __i16 int16_t
+typedef __i8  int8_t
+typedef __u64 size_t
 #endif
 
 #if !defined(__cplusplus) && defined(__GNUC__)
