364b97a983b0f55a18f8e42a3d34526cf25a55cc
Glenn Fiedler <glenn@networknext.com>
2025-11-14 12:20:34 -0500

    design for client pings

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"os/signal"
 	"syscall"
+	"runtime"
 )
 
 var cmd *exec.Cmd
@@ -106,7 +107,11 @@ func help() {
 }
 
 func client_backend() {
-	bash("cd dist && sudo CLIENT_BACKEND_PUBLIC_ADDRESS=45.250.253.243:40000 CLIENT_BACKEND_PRIVATE_KEY=otPzITpGBQbhk0F4u19zjobra/ez4F5YGDekxQI+HFw= ./client_backend")
+	if runtime.GOOS == "linux" {
+		bash("cd dist && sudo CLIENT_BACKEND_PUBLIC_ADDRESS=45.250.253.243:40000 CLIENT_BACKEND_PRIVATE_KEY=otPzITpGBQbhk0F4u19zjobra/ez4F5YGDekxQI+HFw= ./client_backend")
+	} else {
+		bash("cd dist && CLIENT_BACKEND_PUBLIC_ADDRESS=45.250.253.243:40000 CLIENT_BACKEND_PRIVATE_KEY=otPzITpGBQbhk0F4u19zjobra/ez4F5YGDekxQI+HFw= ./client_backend")
+	}
 }
 
 func client() {
