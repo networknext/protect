f1b264fb3aa7cc5a3081c6680d1f53d8453e644b
Glenn Fiedler <glenn@networknext.com>
2025-11-16 10:18:24 -0500

    fix int types

--- cmd/server_xdp/server_xdp_xdp.c
+++ cmd/server_xdp/server_xdp_xdp.c
@@ -50,21 +50,21 @@
 #define bpf_htons(x)        (x)
 #endif // #if NEXT_LITTLE_ENDIAN
 
-void endian_fix( uint16_t * value )
+void endian_fix( __u16 * value )
 {
 #if NEXT_BIG_ENDIAN
     __builtin_bswap16( *value );
 #endif // #if NEXT_BIG_ENDIAN
 }
 
-void endian_fix( uint32_t * value )
+void endian_fix( __u32 * value )
 {
 #if NEXT_BIG_ENDIAN
     __builtin_bswap32( *value );
 #endif // #if NEXT_BIG_ENDIAN
 }
 
-void endian_fix( uint64_t * value )
+void endian_fix( __u64 * value )
 {
 #if NEXT_BIG_ENDIAN
     __builtin_bswap64( *value );
