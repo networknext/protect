d60dd26bf56ea4b931f59e9271da542a195949e3
Glenn Fiedler <glenn@networknext.com>
2025-11-21 10:22:39 -0500

    go

--- sdk/next_server.cpp
+++ sdk/next_server.cpp
@@ -1456,16 +1456,16 @@ void next_server_receive_packets( next_server_t * server )
 
                 next_info( "received %d byte packet on queue %d", packet_bytes, queue );
 
-                if ( packet_bytes > 18 && socket->receive_buffer->num_packets < NEXT_XDP_RECV_QUEUE_SIZE )
+                if ( packet_bytes > 18 && receive_buffer->num_packets < NEXT_XDP_RECV_QUEUE_SIZE )
                 {
-                    const int index = socket->receive_buffer.num_packets++;
+                    const int index = receive_buffer.num_packets++;
 
                     // todo: extract from address from ip and udp headers
-                    next_address_parse( &socket->receive_buffer.from[index], "192.168.1.3:30000" );
+                    next_address_parse( &receive_buffer.from[index], "192.168.1.3:30000" );
                     
-                    socket->receive_buffer.packet_data[index] = socket->receive_buffer.data + index * NEXT_MAX_PACKET_BYTES;
-                    socket->receive_buffer.packet_bytes[index] = packet_bytes;
-                    memcpy( socket->receive_buffer.packet_data[index], packet_data, packet_bytes );
+                    receive_buffer.packet_data[index] = socket->receive_buffer.data + index * NEXT_MAX_PACKET_BYTES;
+                    receive_buffer.packet_bytes[index] = packet_bytes;
+                    memcpy( receive_buffer.packet_data[index], packet_data, packet_bytes );
                 }
 
                 // todo: batch prod__submit -> num_packets
