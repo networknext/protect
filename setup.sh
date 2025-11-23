15595a673243a8634d1e320bb1c017fde7cbd17a
Glenn Fiedler <glenn@networknext.com>
2025-11-22 22:35:10 -0500

    here goes nothing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo ethtool -L enp8s0f0 combined 8
-sudo bash -c "echo 256 > /sys/class/net/enp8s0f0/busy_budget"
-sudo bash -c "echo 2 > /sys/class/net/enp8s0f0/napi_defer_hard_irqs"
-sudo bash -c "echo 200000 > /sys/class/net/enp8s0f0/gro_flush_timeout"
+echo 256 | sudo tee /sys/class/net/enp8s0f0/busy_budget
+echo 2 | sudo tee /sys/class/net/enp8s0f0/napi_defer_hard_irqs
+echo 200000 | sudo tee /sys/class/net/enp8s0f0/gro_flush_timeout
