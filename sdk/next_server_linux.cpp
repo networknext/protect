32661c9598dd3e02f144fe2271e224fad67f029c
Glenn Fiedler <glenn@networknext.com>
2025-11-26 16:51:11 -0500

    again

--- sdk/next_server_linux.cpp
+++ sdk/next_server_linux.cpp
@@ -1381,7 +1381,7 @@ static void xdp_send_thread_function( void * data )
 
         if ( num_completed != 0 )
         {
-            next_info( "marked %d send frames completed on queue %d", num_completed, socket->queue );
+            // next_info( "marked %d send frames completed on queue %d", num_completed, socket->queue );
 
             for ( int i = 0; i < num_completed; i++ )
             {
@@ -1473,7 +1473,7 @@ static void xdp_send_thread_function( void * data )
             // setup descriptors for packets in batch to be sent
 
             // todo
-            next_info( "sent batch of %d packets on queue %d", batch_packets, socket->queue );
+            // next_info( "sent batch of %d packets on queue %d", batch_packets, socket->queue );
 
             for ( int i = 0; i < batch_packets; i++ )
             {
