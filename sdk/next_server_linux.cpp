13b296126e9d60ecf961fcd3d4d8034bb36c1862
Glenn Fiedler <glenn@networknext.com>
2025-11-26 17:49:42 -0500

    go

--- sdk/next_server_linux.cpp
+++ sdk/next_server_linux.cpp
@@ -664,7 +664,7 @@ next_server_t * next_server_create( void * context, const char * bind_address_st
         xsk_config.rx_size = NEXT_XDP_RECV_QUEUE_SIZE;
         xsk_config.tx_size = NEXT_XDP_SEND_QUEUE_SIZE;
         xsk_config.xdp_flags = XDP_ZEROCOPY;     
-        xsk_config.bind_flags = XDP_USE_NEED_WAKEUP;
+        xsk_config.bind_flags = 0; // XDP_USE_NEED_WAKEUP;
         xsk_config.libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD;
 
         result = xsk_socket__create( &socket->xsk, interface_name, queue, socket->umem, &socket->receive_queue, &socket->send_queue, &xsk_config );
