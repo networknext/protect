801529df4889984159682f6d20db85a417099b38
Glenn Fiedler <glenn@networknext.com>
2025-11-26 17:32:58 -0500

    set gateway to client eth addr

--- sdk/next_constants.h
+++ sdk/next_constants.h
@@ -21,7 +21,7 @@
 #define NEXT_XDP_SEND_QUEUE_SIZE                                     4096
 #define NEXT_XDP_RECV_QUEUE_SIZE                                     4096
 #define NEXT_XDP_FILL_QUEUE_SIZE                                     2048
-#define NEXT_XDP_SEND_BATCH_SIZE                                       64
+#define NEXT_XDP_SEND_BATCH_SIZE                                     1024
 
 #else // #ifdef __linux__
 
