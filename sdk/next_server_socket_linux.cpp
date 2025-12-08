81a31f2e2a34d0466e0770e467f0da23b548fd36
Glenn Fiedler <glenn@networknext.com>
2025-12-08 09:52:26 -0500

    try without zero copy

--- sdk/next_server_socket_linux.cpp
+++ sdk/next_server_socket_linux.cpp
@@ -687,7 +687,7 @@ next_server_socket_t * next_server_socket_create( void * context, const char * p
 
         xsk_config.rx_size = NEXT_XDP_RECV_QUEUE_SIZE;
         xsk_config.tx_size = NEXT_XDP_SEND_QUEUE_SIZE;
-        xsk_config.xdp_flags = XDP_ZEROCOPY;     
+        xsk_config.xdp_flags = 0; // XDP_ZEROCOPY;     
         xsk_config.bind_flags = XDP_USE_NEED_WAKEUP;
         xsk_config.libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD;
 
